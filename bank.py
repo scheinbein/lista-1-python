7ebef58a5080e8f553c3ac1304e3dfe469187b42
scheinbein <150675203+scheinbein@users.noreply.github.com>
2023-11-13 08:35:06 -0300

    bank.py

---
+++ bank.py
@@ -0,0 +1,8 @@
+saudacao = input("Você chega no banco e diz: ").lower().strip()
+
+if saudacao.startswith ('hello'):
+    print('$0')
+elif saudacao.startswith('h') and saudacao.startswith != ('hello'):
+    print('$20')
+else: 
+    print('$100')
